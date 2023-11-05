94d213827d5a54bae56e5567952f5bda2aff5aa4
mahe1999mh <mahe1999mh@gmail.com>
2023-11-05 23:02:02 +0530

    updata user

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -16,7 +16,7 @@ router.post('/signup', (req, res) => {
     });
   });
 
-// POST: User Login
+
 router.post('/login', (req, res) => {
     const { username, password } = req.headers;
     db.query('SELECT * FROM users WHERE username = ? AND password = ?', [username, password], (err, results) => {
@@ -31,5 +31,33 @@ router.post('/login', (req, res) => {
       }
     });
 });
+
+router.get('/projects', (req, res) => {
+  db.query('SELECT * FROM project', (err, results) => {
+    if (err) {
+      console.error('Error retrieving projects from the database:', err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+    res.json({ projects: results });
+  });
+});
+
+
+router.get('/projects/:projectId', (req, res) => {
+  const projectId = req.params.projectId;
+
+  db.query('SELECT * FROM project WHERE projectid = ?', [projectId], (err, results) => {
+    if (err) {
+      console.error('Error retrieving the project from the database:', err);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+
+    if (results.length === 0) {
+      return res.status(404).json({ message: 'Project not found' });
+    }
+    res.json({ project: results[0] });
+  });
+});
+
 
 module.exports = router;
