4e03c9f15d92b62dcd7e1533df2acc202126c5fe
mahe1999mh <mahe1999mh@gmail.com>
2023-11-10 23:05:52 +0530

    updated store

--- my-app/src/pages/store.js
+++ my-app/src/pages/store.js
@@ -1,12 +1,11 @@
-import { configureStore ,getDefaultMiddleware} from '@reduxjs/toolkit';
+import { configureStore } from '@reduxjs/toolkit';
+import { projectsApi } from "./store/projectsApi";
 
-import {projectsApi} from "./store/projectsApi"
-
- const store = configureStore({
-    reducer:{
-      [projectsApi.reducerPath]:projectsApi.reducer
-    },
-    middleware:(getDefaultMiddleware) => getDefaultMiddleware().concat(projectsApi.middleware)
-})
+const store = configureStore({
+  reducer: {
+    [projectsApi.reducerPath]: projectsApi.reducer
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }).concat(projectsApi.middleware)
+});
 
 export default store;
