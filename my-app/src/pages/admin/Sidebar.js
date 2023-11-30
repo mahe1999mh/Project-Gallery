dadea3c4e315f9e1d3f55ef2c2d3077d14e7624b
mahe1999mh <mahe1999mh@gmail.com>
2023-11-30 23:21:57 +0530

    sibebar

--- my-app/src/pages/admin/Sidebar.js
+++ my-app/src/pages/admin/Sidebar.js
@@ -1,13 +1,44 @@
 import React, { useState } from 'react';
-import { Drawer, AppBar, Toolbar, IconButton, Typography, List, ListItem, ListItemText, Divider } from '@mui/material';
+import {
+  Drawer,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Divider,
+  ListItemIcon,
+} from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import PersonIcon from '@mui/icons-material/Person';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import BookIcon from '@mui/icons-material/Book';
+import LoginIcon from '@mui/icons-material/Login';
+import NotFoundIcon from '@mui/icons-material/Error';
 
 const Sidebar = () => {
   const [open, setOpen] = useState(false);
+  const [selectedIndex, setSelectedIndex] = useState(0);
 
   const handleDrawerToggle = () => {
     setOpen(!open);
   };
+
+  const handleListItemClick = (index) => {
+    setSelectedIndex(index);
+  };
+
+  const menuItems = [
+    { text: 'Dashboard', icon: <DashboardIcon />, route: '/' },
+    { text: 'User', icon: <PersonIcon />, route: '/user' },
+    { text: 'Product', icon: <ShoppingCartIcon />, route: '/products' },
+    { text: 'Blog', icon: <BookIcon />, route: '/blog' },
+    { text: 'Login', icon: <LoginIcon />, route: '/login' },
+    { text: 'Not Found', icon: <NotFoundIcon />, route: '/404' },
+  ];
 
   return (
     <div>
@@ -27,21 +58,19 @@ const Sidebar = () => {
         </Toolbar>
       </AppBar>
 
-      <Drawer
-        anchor="left"
-        open={open}
-        onClose={handleDrawerToggle}
-      >
+      <Drawer anchor="left" open={open} onClose={handleDrawerToggle}>
         <List>
-          <ListItem button>
-            <ListItemText primary="Home" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="About" />
-          </ListItem>
-          <ListItem button>
-            <ListItemText primary="Contact" />
-          </ListItem>
+          {menuItems.map((item, index) => (
+            <ListItem
+              button
+              key={index}
+              selected={selectedIndex === index}
+              onClick={() => handleListItemClick(index)}
+            >
+              <ListItemIcon>{item.icon}</ListItemIcon>
+              <ListItemText primary={item.text} />
+            </ListItem>
+          ))}
         </List>
 
         <Divider />
@@ -50,7 +79,7 @@ const Sidebar = () => {
       </Drawer>
 
       {/* Main content area */}
-      <div style={{ marginLeft: open ? 340 : 0, width:'20%', padding: '20px', transition: 'margin-left 0.3s' }}>
+      <div style={{ marginLeft: open ? 340 : 0, padding: '20px', transition: 'margin-left 0.3s' }}>
         {/* Your main content goes here */}
         <Typography variant="h4">Welcome to Your App</Typography>
         <p>This is your main content area.</p>
