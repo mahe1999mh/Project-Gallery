a63ba65614f4b93eb0b2512fe5bb35f4ef039ff6
Mahendra Reddy <122714066+mahe1999mh@users.noreply.github.com>
2023-12-13 12:55:54 +0530

    Update App.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -148,3 +148,90 @@ export default App;
 // };
 
 // export default App;
+
+
+// import React from "react";
+// import { Navigate, Outlet, Route, Routes, useRoutes } from "react-router-dom";
+// import SignIn from "./pages/admin/Login";
+// import Sidebar from "./pages/admin/Sidebar";
+// import Users from "./pages/admin/User";
+// import CreateProjectForm from "./pages/admin/CreateProjectForm/CreateProjectForm";
+// import Product from "./pages/admin/Product";
+// import Blog from "./pages/admin/Blog";
+
+// const AdminLayout = () => (
+//   <>
+//     <Sidebar />
+//     <Outlet />
+//   </>
+// );
+
+// const PrivateRoute = ({ children, ...rest }) => {
+//   const token = localStorage.getItem("token");
+//   // Use lazy loading for private routes to avoid initial bundle bloat
+//   const PrivateElement = React.lazy(() => Promise.resolve({ default: children }));
+//   if (!token) {
+//     return <Navigate to="/login" />;
+//   }
+//   return <Route {...rest} element={<PrivateElement />} />;
+// };
+
+// const App = () => {
+//   const routes = [
+//     {
+//       path: "/",
+//       element: <SignIn />,
+//     },
+//     {
+//       path: "/login",
+//       element: <SignIn />,
+//     },
+//     {
+//       path: "/admin/dashboard",
+//       element: <AdminLayout />,
+//       children: [
+//         {
+//           path: "users",
+//           element: (
+//             <PrivateRoute>
+//               <Users />
+//             </PrivateRoute>
+//           ),
+//         },
+//         {
+//           path: "createProject",
+//           element: (
+//             <PrivateRoute>
+//               <CreateProjectForm />
+//             </PrivateRoute>
+//           ),
+//         },
+//         {
+//           path: "products",
+//           element: (
+//             <PrivateRoute>
+//               <Product />
+//             </PrivateRoute>
+//           ),
+//         },
+//         {
+//           path: "blog",
+//           element: (
+//             <PrivateRoute>
+//               <Blog />
+//             </PrivateRoute>
+//           ),
+//         },
+//       ],
+//     },
+//   ];
+
+//   // Use memoization to prevent unnecessary rerenders of routes
+//   const memoizedRoutes = React.useMemo(() => routes, [routes]);
+//   const router = useRoutes(memoizedRoutes);
+
+//   return <div>{router}</div>;
+// };
+
+// export default App;
+
