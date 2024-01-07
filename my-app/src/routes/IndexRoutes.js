e21c596f5acaad0dee438f7beaa0096808d891c7
mahe1999mh <mahe1999mh@gmail.com>
2024-01-07 22:22:57 +0530

    fixed bug

--- my-app/src/routes/IndexRoutes.js
+++ my-app/src/routes/IndexRoutes.js
@@ -1,9 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import UserRoutes from "./UserRoutes";
 import AuthenticationRoutes from "./AuthenticationRoutes";
 
 const IndexRoutes = () => {
-  const pathname = window.location.pathname.split("/")[1];
+  const [pathname, setPathname] = useState(
+    window.location.pathname.split("/")[1]
+  );
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setPathname(window.location.pathname.split("/")[1]);
+    }, 1000);
+
+    return () => clearInterval(intervalId);
+  }, []);
+
   let renderRoutes;
 
   if (pathname !== "admin" || pathname === "login" || pathname === "") {
